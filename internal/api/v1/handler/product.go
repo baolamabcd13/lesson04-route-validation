3ffc0727bf6b00aa314b2ba0e09202be1be0f2cb
baolamabcd13 <nguyenhblam000@gmail.com  git config --global user.name baolamabcd13git config --global user.email nguyenhblam000@gmail.com>
2025-07-31 11:06:27 +0700

    #17

--- internal/api/v1/handler/product.go
+++ internal/api/v1/handler/product.go
@@ -1,7 +1,9 @@
 package v1handler
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"lesson03-route-group/utils"
 	"net/http"
 	"time"
@@ -13,11 +15,37 @@ type GetProductBySLugV1Param struct {
 	Slug string `uri:"slug" binding:"slug,min=5,max=100"`
 }
 
+type ProductsImage struct {
+	ImageName string `json:"image_name" binding:"required"`
+	ImageLink string `json:"image_link" binding:"required,file_ext=jpg png gif"`
+}
+
+type ProductsInfo struct {
+	InfoKey   string `json:"info_key" binding:"required"`
+	InfoValue string `json:"info_value" binding:"required"`
+}
+
 type GetProductsV1Param struct {
 	Search string `form:"search" binding:"required,min=5,max=100,search"`
 	Limit  int    `form:"limit" binding:"omitempty,gte=1,lte=100"`
 	Email  string `form:"email" binding:"omitempty,email"`
 	Date   string `form:"date" binding:"omitempty,datetime=2006-01-02"` //year-month-day
+}
+
+type ProductAttribute struct {
+	AttributeName  string `json:"attribute_name" binding:"required"`
+	AttributeValue string `json:"attribute_value" binding:"required"`
+}
+
+type PostProductsV1Param struct {
+	Name             string                  `json:"name" binding:"required,min=3,max=100"`
+	Price            int                     `json:"price" binding:"required,max_int=100000"`
+	Display          *bool                   `json:"display" binding:"omitempty"`
+	ProductImage     ProductsImage           `json:"product_image" binding:"required"`
+	Tags             []string                `json:"tags" binding:"required,gt=3,lt=5"`
+	ProductAttribute []ProductAttribute      `json:"product_attribute" binding:"required,gt=0,dive"`
+	ProductsInfo     map[string]ProductsInfo `json:"products_info" binding:"required,gt=0,dive"`
+	ProductMetadata  map[string]any          `json:"product_metadata" binding:"omitempty"`
 }
 
 func NewProductHandler() *ProductHandler {
@@ -68,14 +96,39 @@ func (ph *ProductHandler) GetProductBySLugV1(ctx *gin.Context) {
 }
 
 func (ph *ProductHandler) PostProductsV1(ctx *gin.Context) {
-	body, err := ctx.GetRawData()
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, "Error read body request")
+	var params PostProductsV1Param
+	if err := ctx.ShouldBindJSON(&params); err != nil {
+		ctx.JSON(http.StatusBadRequest, utils.HandleValidationErrors(err))
+
+		return
+	}
+
+	for key := range params.ProductsInfo {
+		if _, err := uuid.Parse(key); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": gin.H{
+					"product_info": fmt.Sprintf("Key %s trong product_info không phải là UUID hợp lệ", key),
+				},
+			})
+			return
+		}
+	}
+
+	if params.Display == nil {
+		defaultDisplay := true
+		params.Display = &defaultDisplay
 	}
 
 	ctx.JSON(http.StatusCreated, gin.H{
-		"message": "product created (V1)",
-		"data":    string(body),
+		"message":           "product created (V1)",
+		"name":              params.Name,
+		"price":             params.Price,
+		"display":           params.Display,
+		"product_image":     params.ProductImage,
+		"tags":              params.Tags,
+		"product_attribute": params.ProductAttribute,
+		"product_info":      params.ProductsInfo,
+		"product_metadata":  params.ProductMetadata,
 	})
 }
 
--- utils/validation.go
+++ utils/validation.go
@@ -5,7 +5,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
+	"path/filepath"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -14,32 +16,60 @@ func HandleValidationErrors(err error) gin.H {
 		errors := make(map[string]string)
 
 		for _, e := range validationError {
+			root := strings.Split(e.Namespace(), ".")[0]
+
+			rawPath := strings.TrimPrefix(e.Namespace(), root+".")
+
+			parts := strings.Split(rawPath, ".")
+
+			for i, part := range parts {
+				if strings.Contains(part, "[") {
+					idx := strings.Index(part, "[")
+					base := camelToSnake(part[:idx]) // tu 0 den -> [
+					index := part[idx:]
+					parts[i] = base + index
+				} else {
+					parts[i] = camelToSnake(part)
+				}
+			}
+
+			filepath := strings.Join(parts, ".")
+
 			switch e.Tag() {
 			case "gt":
-				errors[e.Field()] = e.Field() + " phải lớn hơn giá trị tối thiểu"
+				errors[filepath] = fmt.Sprintf("%s phải lớn hơn %s", filepath, e.Param())
+			case "lt":
+				errors[filepath] = fmt.Sprintf("%s phải nhỏ hơn %s", filepath, e.Param())
 			case "uuid":
-				errors[e.Field()] = e.Field() + " phải là uuid hợp lệ"
+				errors[filepath] = fmt.Sprintf("%s phải là uuid hợp lệ", filepath)
 			case "slug":
-				errors[e.Field()] = e.Field() + " chỉ được chưa chữ thuờng, số, dấu gạch ngang hoặc dấu chấm"
+				errors[filepath] = fmt.Sprintf("%s chỉ được chưa chữ thuờng, số, dấu gạch ngang hoặc dấu chấm", filepath)
 			case "min":
-				errors[e.Field()] = fmt.Sprintf("%s phải nhiều hơn %s ký tự", e.Field(), e.Param())
+				errors[filepath] = fmt.Sprintf("%s phải nhiều hơn %s ký tự", filepath, e.Param())
 			case "max":
-				errors[e.Field()] = fmt.Sprintf("%s phải ít hơn %s ký tự", e.Field(), e.Param())
+				errors[filepath] = fmt.Sprintf("%s phải ít hơn %s ký tự", filepath, e.Param())
 			case "oneof":
 				allowedValues := strings.Join(strings.Split(e.Param(), " "), ",")
-				errors[e.Field()] = fmt.Sprintf("%s phải là một trong các giá trị: %s", e.Field(), allowedValues)
+				errors[filepath] = fmt.Sprintf("%s phải là một trong các giá trị: %s", filepath, allowedValues)
 			case "required":
-				errors[e.Field()] = e.Field() + " là bắt buộc"
+				errors[filepath] = fmt.Sprintf("%s là bắt buộc", filepath)
 			case "search":
-				errors[e.Field()] = e.Field() + " chỉ được chưa chữ thuờng, in hoa, số và khoảng trắng"
+				errors[filepath] = fmt.Sprintf("%s chỉ được chưa chữ thuờng, in hoa, số và khoảng trắng", filepath)
 			case "gte":
-				errors[e.Field()] = e.Field() + " phải lớn hơn hoặc bằng giá trị tối thiêu"
+				errors[filepath] = fmt.Sprintf("%s phải lớn hơn hoặc bằng %s", filepath, e.Param())
 			case "lte":
-				errors[e.Field()] = e.Field() + " phải nhỏ hơn hoặc bằng giá trị tối thiêu"
+				errors[filepath] = fmt.Sprintf("%s phải nhỏ hơn hoặc bằng %s", filepath, e.Param())
 			case "email":
-				errors[e.Field()] = e.Field() + " phải đúng định dạng là email"
+				errors[filepath] = fmt.Sprintf("%s phải đúng định dạng là email", filepath)
 			case "datetime":
-				errors[e.Field()] = e.Field() + " phải theo đúng định dạng YYYY-MM-DD"
+				errors[filepath] = fmt.Sprintf("%s phải theo đúng định dạng YYYY-MM-DD", filepath)
+			case "min_int":
+				errors[filepath] = fmt.Sprintf("%s phải có giá trị lớn hơn %s", filepath, e.Param())
+			case "max_int":
+				errors[filepath] = fmt.Sprintf("%s phải có giá trị bé hơn %s", filepath, e.Param())
+			case "file_ext":
+				allowedValues := strings.Join(strings.Split(e.Param(), " "), ",")
+				errors[filepath] = fmt.Sprintf("%s chỉ cho phép nhũng file có extension là: %s", filepath, allowedValues)
 			}
 		}
 
@@ -64,5 +94,47 @@ func RegisterValidators() error {
 	v.RegisterValidation("search", func(fl validator.FieldLevel) bool {
 		return searchgRegex.MatchString(fl.Field().String())
 	})
+
+	v.RegisterValidation("min_int", func(fl validator.FieldLevel) bool {
+		minStr := fl.Param()
+		//base
+		//10: hệ thập phân decimal
+		//16: hệ thâp lục phân hex -> FF = 255
+		//2: hệ nhị phân binary -> 1010 = 10
+		minVal, err := strconv.ParseInt(minStr, 10, 64)
+		if err != nil {
+			return false
+		}
+		return fl.Field().Int() >= minVal
+	})
+
+	v.RegisterValidation("max_int", func(fl validator.FieldLevel) bool {
+		maxStr := fl.Param()
+		maxVal, err := strconv.ParseInt(maxStr, 10, 64)
+		if err != nil {
+			return false
+		}
+		return fl.Field().Int() <= maxVal
+	})
+
+	//file_ext=jpg,png
+	v.RegisterValidation("file_ext", func(fl validator.FieldLevel) bool {
+		filename := fl.Field().String()
+		allowedStr := fl.Param()
+		if allowedStr == "" {
+			return false
+		}
+
+		allowedExt := strings.Fields(allowedStr)
+		ext := strings.TrimPrefix(strings.ToLower(filepath.Ext(filename)), ".")
+
+		for _, allowed := range allowedExt {
+			if ext == strings.ToLower(allowed) {
+				return true
+			}
+		}
+		return false
+	})
+
 	return nil
 }
